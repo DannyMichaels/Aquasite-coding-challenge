bd9eec717fbbe8623adc76ceae66746ec0864607
Daniel Michael <itzdanielmichael@gmail.com>
2021-04-21 22:38:43 -0400

    AppRouter

--- client/src/router/AppRouter.js
+++ client/src/router/AppRouter.js
@@ -1,8 +1,11 @@
 import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
 
 const AppRouter = () => (
   <Switch>
-    <Route path="/" />
+    {/* <Route exact path="/login" component={Login} /> */}
+    {/* <Route exact path="/register" component={Register} /> */}
+    {/* <PrivateRoute exact path="/" component={Home} /> */}
   </Switch>
 );
 
